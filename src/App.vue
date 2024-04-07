6c05263da4b2adba0e337d87ece5c2dfc87c007b
aleksjelic <aleksandrajelic22@gmail.com>
2024-04-07 12:44:24 +0200

    I added all files from project

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<script setup>
+import {ref} from 'vue'
+let counter=ref(0)
+console.log(counter)
+const addToCounter=()=>{
+counter.value=counter.value+1
+console.log("HELLO FROM ADD TO COUNTER")
+}
+const decreaseCounter=()=>{
+  console.log("HELLO FROM DECREASE")
+  counter.value=counter.value-1
+}
+
+</script>
+
+<template>
+  <main>
+  <div>
+<h4>
+  The current count is...
+</h4>
+<h1>{{ counter}}</h1>
+<button @click="decreaseCounter">-</button>
+<button @click="addToCounter">+</button>
+</div>
+</main>
+
+  
+</template>
+
+<style scoped>
+main {
+  background-color: aliceblue;
+  width: 100wh;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  text-align: center;
+}
+
+</style>
